5b48cd7d9e2068bb37c9513772fe3203085d1413
polina <polinatuzankina@gmail.com>
2023-07-09 20:44:31 +0600

    module 3 final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int ones = number % 10;
+        int tens = number / 10 % 10;
+        int hundreds = number / 10 / 10 % 10;
+        int thousands = number / 10 / 10 / 10 % 10;
+
+        System.out.println(ones + tens + hundreds + thousands);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int ones = number % 10;
+        int tens = number / 10 % 10;
+        int hundreds = number / 10 / 10 % 10;
+
+        System.out.println(ones * 100 + tens * 10 + hundreds);
     }
 }
